515532064265a0f17d271a30d6293a44e06e0060
Arthur <arthur.omar@numericoach.fr>
2026-07-23 13:16:32 +0200

    chore: clean up temporary test files and old scripts

--- read_params.js
+++
@@ -1,9 +0,0 @@
-const xlsx = require('xlsx');
-const workbook = xlsx.readFile('LEROY MERLIN - TABLEAU DE BORD - FORMATIONS.xlsx');
-const sheet = workbook.Sheets['PARAMETRES'];
-const data = xlsx.utils.sheet_to_json(sheet, { header: 1 });
-data.forEach((row, i) => {
-    if (row && row[0]) {
-        console.log(`Row ${i}: ${row[0]} | ${row[1]}`);
-    }
-});
